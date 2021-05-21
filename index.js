e3be20de99afe304124be5920c73a1b0bdb7e153
kikelhl <lhlkike@gmail.com>
2021-05-20 22:50:47 -0500

    Avance 10:50pm

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
+const morgan = require('morgan');
 const express = require('express');
 const app = express();
+const usuarios = require('./routes/usuarios')
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res, next) => {
-    res.status(200);
-    res.send("Bienvenido");
+    return res.status(200).send("Bienvenido al sistema");
 });
 
-app.listen(3000, () => {
+app.use("/usuarios", usuarios);
+
+app.listen(process.env.PORT || 3000, () => {
     console.log("Server is running... ");
 });
---
+++ routes/usuarios.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const usuarios = express.Router();
+const db = require('../config/database');
+
+usuarios.post("/", (req, res, next) => {
+    return res.status(200).json(req.body);
+});
+
+usuarios.get('/', async(req, res, next) => {
+    const usr = await db.query("SELECT * FROM users");
+    return res.status(200).json(usr);
+});
+
+usuarios.get('/:id([0-9]{1,3})', async (req, res, next) => {
+    const id = req.params.id;
+    if (id >= 0 && id <= 2) {
+        const usr = await db.query("SELECT * FROM users WHERE id = "+id+";")
+        return res.status(200).json(usr)
+    }
+    return res.status(404).json({code: 404, message: "Usuario no encontrado"});
+    
+});
+
+usuarios.get('/:name([A-Za-z]+)', async (req, res, next) => {
+    const name = req.params.name;
+    const usr = await db.query("SELECT * FROM users WHERE name ='"+name+"';");
+    if (usr.length > 0) {
+        return res.status(200).json(usr);
+    }
+    return res.status(404).send("Usuario no encontrado");
+        
+});
+
+module.exports = usuarios;
