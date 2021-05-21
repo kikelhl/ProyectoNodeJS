c7befb2da706c7fe0e223bcb6c1807bb7a93c531
kikelhl <lhlkike@gmail.com>
2021-05-20 23:05:55 -0500

    Terminado hasta Act 8

--- index.js
+++ index.js
@@ -8,10 +8,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res, next) => {
-    return res.status(200).send("Bienvenido al sistema");
+    return res.status(200).json({code: 200, message: "Bienvenido al sistema" });
 });
 
 app.use("/usuarios", usuarios);
+
+app.use((req, res, next) => {
+    return res.status(404).json({code: 404, message: "URL no encontrado" });
+});
 
 app.listen(process.env.PORT || 3000, () => {
     console.log("Server is running... ");
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -8,14 +8,14 @@ usuarios.post("/", (req, res, next) => {
 
 usuarios.get('/', async(req, res, next) => {
     const usr = await db.query("SELECT * FROM users");
-    return res.status(200).json(usr);
+    return res.status(200).json({code: 200, message: usr });
 });
 
 usuarios.get('/:id([0-9]{1,3})', async (req, res, next) => {
     const id = req.params.id;
-    if (id >= 0 && id <= 2) {
+    if (id >= 1 && id <= 2) {
         const usr = await db.query("SELECT * FROM users WHERE id = "+id+";")
-        return res.status(200).json(usr)
+        return res.status(200).json({code: 200, message: usr})
     }
     return res.status(404).json({code: 404, message: "Usuario no encontrado"});
     
@@ -25,9 +25,9 @@ usuarios.get('/:name([A-Za-z]+)', async (req, res, next) => {
     const name = req.params.name;
     const usr = await db.query("SELECT * FROM users WHERE name ='"+name+"';");
     if (usr.length > 0) {
-        return res.status(200).json(usr);
+        return res.status(200).json({code: 200, message: usr})
     }
-    return res.status(404).send("Usuario no encontrado");
+    return res.status(404).json({code: 404, message: "Usuario no encontrado"});
         
 });
 
