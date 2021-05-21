b94018f5715479647365440d2ee19505265709d7
kikelhl <lhlkike@gmail.com>
2021-05-20 23:41:37 -0500

    Terminado hasta Act 10

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -20,6 +20,55 @@ usuarios.post("/", async (req, res, next) => {
     return res.status(500).json({ code: 500, message: "Campos incompletos" });
 });
 
+usuarios.delete('/:id([0-9]{1,3})', async (req, res, next) => {
+
+    const query = `DELETE FROM users WHERE id = ${req.params.id};`;
+    const rows = await db.query(query);
+
+    if (rows.affectedRows == 1) {
+        return res.status(200).json({ code: 200, message: "Usuario borrado correctamente" });
+    }
+    return res.status(404).json({ code: 404, message: "Usuario no encontrado" });
+
+});
+
+usuarios.put('/:id([0-9]{1,3})', async (req, res, next) => {
+    const {name, last_name, number, email, address } = req.body;
+
+    if (name && last_name && number && email && address) {
+        let query = `UPDATE users SET name= '${name}', last_name= '${last_name}', number= '${email}', address='${address}' `;
+        query += `WHERE id = ${req.params.id};`;
+
+        const rows = await db.query(query)
+
+        if (rows.affectedRows == 1) {
+            return res.status(200).json({ code: 200, message: "Usuario actualizado correctamente" });
+        }
+
+        return res.status(500).json({ code: 500, message: "Ocurrió un error" });
+
+    }
+    return res.status(500).json({ code: 500, message: "Campos incompletos" });
+
+
+});
+
+usuarios.patch('/:id([0-9]{1,3})', async (req, res, next) => {
+
+    if (req.body.name) {
+        let query = `UPDATE users SET name= '${req.body.name}' WHERE id = ${req.params.id}`;
+
+        const rows = await db.query(query);
+
+        if (rows.affectedRows == 1) {
+            return res.status(200).json({ code: 200, message: "Usuario actualizado correctamente" });
+        }
+        return res.status(500).json({ code: 500, message: "Usuario no encontrado" });
+    }
+    return res.status(500).json({ code: 500, message: "Campos incompletos" });
+
+});
+
 usuarios.get('/', async(req, res, next) => {
     const usr = await db.query("SELECT * FROM users");
     return res.status(200).json({code: 200, message: usr });
