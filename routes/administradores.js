216a48fbc536b4d1c332389a061e2956bdde0128
kikelhl <lhlkike@gmail.com>
2021-05-21 00:35:00 -0500

    Terminado hasta Act 12

--- routes/administradores.js
+++ routes/administradores.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const administradores = express.Router();
 const db = require('../config/database');
 
@@ -16,6 +17,26 @@ administradores.post("/", async (req, res, next) => {
         return res.status(500).json({ code: 500, message: "Ocurrió un error" });
     }  
     return res.status(500).json({code: 500, message: "Campos incompletos"});
+});
+
+administradores.post("/login", async (req, res, next) => {
+    const {admin_email, admin_password} = req.body;
+    const query = `SELECT * from admins WHERE admin_email = '${admin_email}' AND admin_password = '${admin_password}';`;
+    const rows = await db.query(query);
+
+    if (admin_email && admin_password) {
+        if(rows.length == 1) {
+            const token = jwt.sign({
+                admin_id: rows[0].admin_id,
+                admin_password: rows[0].admin_password
+            }, "debugkey");
+            return res.status(200).json({code: 200, message: token});
+        }
+        else {
+            return res.status(401).json({code: 401, message: "Usuario Y/O contraseña incorrectos"});
+        }
+    }
+    return res.status(401).json({code: 500, message: "Campos incompletos"});    
 });
 
 administradores.get("/", async (req, res, next) => {
